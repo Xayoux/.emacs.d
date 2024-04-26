da8efd8cd6e1433a38e2fb9289792fff4281e075
Romain CAPLIEZ <romain.capliez01@gmail.com>
2024-04-26 16:09:55 +0200

    Ajout des smarts parentheses

--- custom.el
+++ custom.el
@@ -10,7 +10,7 @@
 		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
  '(org-return-follows-link t)
  '(package-selected-packages
-   '(org-super-agenda nerd-icons all-the-icons olivetti rainbow-delimiters ligature nerd-icons-completion nerd-icons-ibuffer nerd-icons-ivy-rich nerd-icons-dired rainbow-mode ivy-rich magit org-roam rutils ess org org-fragtog org-modern org-appear cdlatex yasnippet ivy-prescient ivy-xref counsel company-box company-reftex company-math company-bibtex company visual-fill-column adaptive-wrap auctex doom-themes doom-modeline)))
+   '(smartparens org-super-agenda nerd-icons all-the-icons olivetti rainbow-delimiters ligature nerd-icons-completion nerd-icons-ibuffer nerd-icons-ivy-rich nerd-icons-dired rainbow-mode ivy-rich magit org-roam rutils ess org org-fragtog org-modern org-appear cdlatex yasnippet ivy-prescient ivy-xref counsel company-box company-reftex company-math company-bibtex company visual-fill-column adaptive-wrap auctex doom-themes doom-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
