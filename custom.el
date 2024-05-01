f820673a9a6043af15263e3405b7f42cee61a7b8
Romain Capliez <romain.capliez01@gmail.com>
2024-05-01 16:53:47 +0200

    ajout checkbox dans custom (maj org modern)

--- custom.el
+++ custom.el
@@ -9,9 +9,10 @@
  '(org-format-latex-options
    '(:foreground default :background default :scale 1.75 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
 		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
+ '(org-modern-checkbox '((88 . "☑") (45 . "□–") (32 . "□")))
  '(org-modern-star "◉○◈◇✳")
  '(package-selected-packages
-   '(olivetti rainbow-delimiters ligature nerd-icons-completion nerd-icons-ibuffer nerd-icons-ivy-rich nerd-icons-dired rainbow-mode ivy-rich magit org-roam rutils ess org org-fragtog org-modern org-appear cdlatex yasnippet ivy-prescient ivy-xref counsel company-box company-reftex company-math company-bibtex company visual-fill-column adaptive-wrap auctex doom-themes doom-modeline)))
+   '(rainbow-delimiters ligature nerd-icons-completion nerd-icons-ibuffer nerd-icons-ivy-rich nerd-icons-dired rainbow-mode ivy-rich magit org-roam rutils ess org org-fragtog org-modern org-appear cdlatex yasnippet ivy-prescient ivy-xref counsel company-box company-reftex company-math company-bibtex company visual-fill-column adaptive-wrap auctex doom-themes doom-modeline)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
